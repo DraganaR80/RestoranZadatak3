6086df6338b23db645027bb8bd678ce4ea3c5e0b
Dragana Reljic <reljicdragana@yahoo.com>
2023-02-23 21:15:59 +0100

    dodata galerija i css

---
+++ view/galerija.php
@@ -0,0 +1,74 @@
+<?php
+require("galerijaHeader.php");
+require("navbar.php");
+
+?>
+<link rel="stylesheet" href="../css/galerija.css">
+<body>
+    <div class="Text">
+
+    <p> Par detalja iz našeg prijatnog ambijenta. Vikendom možete uživati uz zvuke tamburaša.
+        <br> 
+        Kada je lepo vreme , opustite se u hladovini bašte našeg retorana.
+    </p> 
+
+    </div>
+<div class="galerija">
+    <div class="restoran">
+    <img  class="img"   src="../images/restoran1.jpg" alt=""width="300" height="250" /> 
+    
+    </div>
+    <div class="restoran">
+        
+    <img  class="img" src="../images/restoran2.jpg" alt="" width="300" height="250"/>
+    
+</div>
+    <div class="restoran">
+        
+        <img class="img"  src="../images/restoran3.jpg" alt="" width="300" height="250" />
+        
+    </div>
+    <div class="restoran">
+        
+        <img class="img"   src="../images/restoran4.jpg" alt=" " width="300" height="250"/>
+        
+    </div>
+    <div class="restoran">
+        
+        <img  class="img"  src="../images/restoran5.jpg" alt="" width="300" height="250"/>
+        
+    </div>
+
+        <div class="restoran">
+        
+            <img class="img"  src="../images/restoran7.jpg" alt="" width="300" height="250"/>
+        
+        </div>
+
+        <div class="restoran">
+        
+            <img  class="img" src="../images/restoran8.jpg" alt="" width="300" height="250"/>
+            
+        </div>
+
+        <div class="restoran">
+        
+            <img class="img" src="../images/restoran9.jpg" alt="" width="300" height="250"/>
+            
+        </div>
+
+        <div class="restoran">
+        
+        <img class="img" src="../images/restoranBasta.jpg" alt="" width="300" height="250"/>
+        
+    </div>
+
+
+
+
+</div>
+
+
+
+</body>
+</html>
--- view/navbar.php
+++ view/navbar.php
@@ -1,9 +1,8 @@
 <div class="header">
 <ul>
 
-<li> O nama</li>
-<li> Galerija</li>
-<li> Kontakt</li>
+<li> <a href="o_nama.php"> O nama</a> </li>
+<li>  <a href="galerija.php">Galerija</a> </li>
 <li> <img src="../images/2611141.png" width="30px" length="30px" > </li>
 <li><h4>NAŠA PRIČA</h4></li>
 
