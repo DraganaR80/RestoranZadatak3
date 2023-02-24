e8d67772cf092054b3763f85acee26764837ada9
Dragana Reljic <reljicdragana@yahoo.com>
2023-02-24 10:28:06 +0100

    otkucan delete ali ne radi

---
+++ controller/obrisiRezervaciju.php
@@ -0,0 +1,39 @@
+<?php
+
+require "../dbBroker.php";
+require "../model/rezervacija.php";
+
+if(isset($_POST['id'])){
+
+//if (isset($_POST['fullName']) 
+//&& isset($_POST['email'])
+//&& isset($_POST['phone'])
+//&& isset ($_POST['noppl'])
+//&& isset ($_POST['date'])
+//&& isset ($_POST['time'])){
+
+    $status= Reservation::deleteById($_POST['id'],$conn);
+   // $_POST['email'],
+    //$_POST['phone'],
+    //$_POST['noppl'],
+    //$_POST['date'],
+    //$_POST['time'],
+   // $conn);
+
+    If($status){
+        header ("Location:../view/potvrda.php");
+        echo "Vaša rezervacija je obrisana!";
+    } else{
+
+        echo "Pokusajte ponovo";
+         header ("Location:../view/potvrda.php");
+    }
+
+}
+
+
+
+
+
+
+?>
